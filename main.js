97ad78594056be25a17186c47208f58236576c96
DonnieAndMooie <owenjeaves@gmail.com>
2023-01-27 00:00:10 +0000

    Complete Knights Travails function

--- main.js
+++ main.js
@@ -1,16 +1,14 @@
-function createGameBoard(){
-    let gameBoard = []
-    for (let i=0; i <= 7; i++){
-        for (let j=0; j<= 7; j++){
-            const newSquare = [i,j]
-            gameBoard.push(newSquare)
-        }
+class Node{
+    constructor(position, distanceFromStart, path){
+        this.position = position
+        this.distanceFromStart = distanceFromStart
+        this.neighbours = findPossibleMoves(position)
+        this.path = path
     }
-    return gameBoard
 }
 
-function knightMoves(start, end){
-    const gameBoard = createGameBoard()
+
+function findPossibleMoves(start){
     let possibleMoves = []
     let newMove = [(start[0]+2), (start[1]-1)]
     possibleMoves.push(newMove)
@@ -35,8 +33,62 @@ function knightMoves(start, end){
             i--
         }
     }
-
-    console.log(possibleMoves)
+    return possibleMoves
 }
 
-knightMoves([0,0])
+
+
+function knightMoves(start, end){
+    const startNode = new Node (start, 0, [])
+    const endNode = new Node (end, 0)
+    const queue = []
+    const visited = []
+    const nodes = []
+    queue.push(startNode)
+    while (queue[0]){
+        let currentNode = queue[0].position
+        if (currentNode.toString() === end.toString()){
+            break
+        }
+        for (const item of visited){
+            if (item.toString() === currentNode.toString()){
+                queue.shift()
+            }
+        }
+        let possibleMoves = findPossibleMoves(currentNode)
+        for (const move of possibleMoves){
+            let distance = queue[0].distanceFromStart + 1
+            if(nodes.filter(item => item.position.toString() === move.toString()).length === 0){
+                let path = JSON.parse(JSON.stringify(queue[0].path))
+                path.push(queue[0].position)
+                const node = new Node(move, distance, path)
+                queue.push(node)
+                nodes.push(node)
+            }
+            }
+        visited.push(currentNode)
+        queue.shift()
+    }
+        output(nodes, end)
+}
+
+function output(nodes, end){
+    let numMoves
+    let path
+    for (const node of nodes){
+        if (node.position.toString() === end.toString()){
+            numMoves = node.distanceFromStart
+            path = node.path
+
+        }
+    }
+    console.log(`You made it in ${numMoves} moves!`)
+    console.log("Here's your path:")
+    for (const square of path){
+        console.log(square)
+    }
+    console.log(end)
+}
+
+knightMoves([0,0], [7,7])
+
